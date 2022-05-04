b7a08d20c2e211db676fd7e3fa2548bd105b0054
KVenLucas <k_l.s@outlook.com>
2022-05-03 23:50:20 -0300

    :chore eslint, prettier, husky, lint staged setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis obcaecati ipsa
+        rerum odit ullam nam molestiae, eos laboriosam tempore architecto corrupti quas
+        quaerat ipsum, unde eius, molestias ipsam quia maiores!
+      </p>
+    </div>
+  );
+}
+
+export default App;
